ab5f07d4521a3a7308928f98fbe7f4c791abe310
MRaifAydin <mert5708@gmail.com>
2022-04-06 14:18:24 +0300

    Drawer Added
    Added drawer for multi page navigation.

---
+++ vr_marketim/lib/drawer_widget.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: const Center(
+        child: Text('My Page!'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('İşlemler'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- vr_marketim/lib/main.dart
+++ vr_marketim/lib/main.dart
@@ -1,16 +1,20 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
+import 'drawer_widget.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static const appTitle = 'VrMarketim Demo';
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: "VrMarketim",
-      home: Text("Deneme"),
+      title: appTitle,
+      home: DrawerWidget(title: appTitle),
     );
   }
 }
