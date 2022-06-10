02b96d3ecfcf77646ea79b73a53805643d71f71c
MRaifAydin <mert5708@gmail.com>
2022-06-11 00:03:11 +0300

    Dynamic table rows.
    Added dynamic table rows.

--- vr_marketim/lib/pages/basket.dart
+++ vr_marketim/lib/pages/basket.dart
@@ -21,8 +21,31 @@ Future<List> fetchAlbum() async {
   }
 }
 
-Widget returnRow(List deneme) {
-  return Container();
+List<DataRow> createRows(List snapshot) {
+  List<DataRow> rows = [];
+  for (int i = 0; i < snapshot.length; ++i) {
+    rows.add(
+      DataRow(
+        cells: <DataCell>[
+          DataCell(Center(
+            child: Text('${snapshot[i].name}'),
+          )),
+          DataCell(Center(
+            child: Text('${snapshot[i].description}'),
+          )),
+          DataCell(Center(
+            child: FloatingActionButton.small(
+              heroTag: i,
+              onPressed: null,
+              child: Icon(Icons.delete),
+              backgroundColor: Colors.red,
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+  return rows;
 }
 
 class BasketTable extends StatefulWidget {
@@ -44,50 +67,68 @@ class _BasketTableState extends State<BasketTable> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Basket")),
-      body: SingleChildScrollView(
-        child: FutureBuilder<List>(
-          future: futureAlbum,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return DataTable(
-                columns: const <DataColumn>[
-                  DataColumn(
-                      label: Expanded(
-                    child: Text(
-                      'Products',
-                      style: TextStyle(fontStyle: FontStyle.italic),
-                      textAlign: TextAlign.center,
-                    ),
-                  )),
-                ],
-                rows: <DataRow>[
-                  DataRow(
-                    cells: <DataCell>[
-                      DataCell(Center(
-                        child: Text('${snapshot.data!.length}'),
+        appBar: AppBar(title: Text("Basket")),
+        body: ConstrainedBox(
+          constraints:
+              BoxConstraints.expand(width: MediaQuery.of(context).size.width),
+          child: SingleChildScrollView(
+            child: FutureBuilder<List>(
+              future: futureAlbum,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return DataTable(
+                    columns: const <DataColumn>[
+                      DataColumn(
+                          label: Expanded(
+                        child: Text(
+                          'Name',
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                          textAlign: TextAlign.center,
+                        ),
                       )),
-                    ],
-                  ),
-                  DataRow(
-                    cells: <DataCell>[
-                      DataCell(Center(
-                        child: Text('${snapshot.data![1].name}'),
+                      DataColumn(
+                          label: Expanded(
+                        child: Text(
+                          'Description',
+                          style: TextStyle(fontStyle: FontStyle.italic),
+                          textAlign: TextAlign.center,
+                        ),
                       )),
+                      DataColumn(
+                          label: Expanded(
+                              child: FloatingActionButton.small(
+                        onPressed: null,
+                        child: Icon(Icons.delete),
+                      ))),
                     ],
-                  ),
-                ],
-              );
-              // return Text(snapshot.data!.name);
-            } else if (snapshot.hasError) {
-              return Text('${snapshot.error}');
-            }
+                    // rows: <DataRow>[
+                    //   DataRow(
+                    //     cells: <DataCell>[
+                    //       DataCell(Center(
+                    //         child: Text('${snapshot.data!.length}'),
+                    //       )),
+                    //     ],
+                    //   ),
+                    //   DataRow(
+                    //     cells: <DataCell>[
+                    //       DataCell(Center(
+                    //         child: Text('${snapshot.data![1].name}'),
+                    //       )),
+                    //     ],
+                    //   ),
+                    // ],
+                    rows: createRows(snapshot.data!),
+                  );
+                  // return Text(snapshot.data!.name);
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
 
-            // By default, show a loading spinner.
-            return const CircularProgressIndicator();
-          },
-        ),
-      ),
-    );
+                // By default, show a loading spinner.
+                return const CircularProgressIndicator();
+              },
+            ),
+          ),
+        ));
   }
 }
